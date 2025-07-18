012bfa208fe304e3525953879108257fe5134c5d
tuan <tuanhoang19042004@gmail.com>
2025-07-18 21:35:31 +0700

    xoá ngthue chủ nha model

---
+++ DA/Controllers/TaiKhoanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DA.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

2e58211c7b971df7a770c32e27695ce53508a3f0
vn-thang <thangvi446@gmail.com>
2025-07-18 22:33:00 +0700

    create Models/ NguoiThue

---
+++ DA/Models/NguoiThue.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class NguoiThue
+{
+    [Key]
+    public int MaNguoiThue { get; set; }
+
+    [Required]
+    public int MaTaiKhoan { get; set; }
+
+    [ForeignKey("MaTaiKhoan")]
+    public virtual TaiKhoan TaiKhoan { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string HoTen { get; set; }
+
+    [Required]
+    [StringLength(12)]
+    public string CCCD { get; set; }
+
+    [Required]
+    [StringLength(15)]
+    public string SoDienThoai { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
